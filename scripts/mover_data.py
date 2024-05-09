ddbc5c9e2d397a66a1bdfef5b2272ac5f3029803
Manuel Pravia <95064609+manuelpravia@users.noreply.github.com>
2024-05-09 17:05:44 -0500

    Update mover_data.py

--- scripts/mover_data.py
+++ scripts/mover_data.py
@@ -1,5 +1,5 @@
 
-archivo_texto = "/opt/airflow/dags/repo/scripts/archivo.txt"
+archivo_texto = "/opt/airflow/data/archivo.txt"
 
 with open(archivo_texto, 'a') as archivo:
     # Escribe una cadena en una nueva línea en el archivo
