84a02338c3a164a190eeb0b72e33b2edfb511fa1
manuelpravia <praviapuri@gmail.com>
2024-05-15 12:51:21 -0500

    add timeout update

--- prueba2_servidor.py
+++ prueba2_servidor.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
